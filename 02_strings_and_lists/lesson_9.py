075eb49d774abee8d8bae9fff572e396f612a255
Kristina Tarakanova <kkanadore@gmail.com>
2024-07-29 14:39:10 +0300

    Task 2

--- 02_strings_and_lists/lesson_9.py
+++ 02_strings_and_lists/lesson_9.py
@@ -1,6 +1,12 @@
 # Task 1
+#
+# lst = [5.4, 6.7, 10.4]
+# digs = list(map(int, input().split()))
+# lst.append(digs)
+# print(lst)
 
-lst = [5.4, 6.7, 10.4]
-digs = list(map(int, input().split()))
-lst.append(digs)
-print(lst)
+
+# Task 2
+
+result = [input().split(), input().split(), input().split()]
+print(result)
