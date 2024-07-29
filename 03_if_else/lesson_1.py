b8ff2916e4fe016a43ee5594c7076243957e19bf
Kristina Tarakanova <kkanadore@gmail.com>
2024-07-29 16:12:02 +0300

    Task 2

--- 03_if_else/lesson_1.py
+++ 03_if_else/lesson_1.py
@@ -1,7 +1,16 @@
 # Task 1
+#
+# a, b = map(float, input().split())
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
 
-a, b = map(float, input().split())
-if a > b:
-    print(a)
+
+# Task 2
+
+word = input().lower()
+if word == word[::-1]:
+    print('ДА')
 else:
-    print(b)
+     print('НЕТ')
