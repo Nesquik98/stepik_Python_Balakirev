0c78d986e4fddae9cf1c8207453c3f790db958aa
Kristina Tarakanova <kkanadore@gmail.com>
2024-07-29 14:52:04 +0300

    Task 4

--- 02_strings_and_lists/lesson_9.py
+++ 02_strings_and_lists/lesson_9.py
@@ -13,6 +13,15 @@
 
 
 # Task 3
+#
+# result = [input().split(), input().split(), input().split()]
+# print(result[0][-1], result[1][-1], result[2][-1])
 
-result = [input().split(), input().split(), input().split()]
-print(result[0][-1], result[1][-1], result[2][-1])
+
+# Task 4
+
+t = [["Скажи-ка", "дядя", "ведь", "не", "даром"],
+     ["Я", "Python", "выучил", "с", "каналом"],
+     ["Балакирев", "что", "раздавал?"]]
+word = input()
+print(word in t[0] or word in t[1] or word in t[2])
