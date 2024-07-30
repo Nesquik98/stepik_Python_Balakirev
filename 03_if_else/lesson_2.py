da3b17d91a098631699dac4e0670214d5e57c280
Kristina Tarakanova <kkanadore@gmail.com>
2024-07-30 19:41:08 +0300

    Task 2

--- 03_if_else/lesson_2.py
+++ 03_if_else/lesson_2.py
@@ -1,26 +1,39 @@
 # Task 1
+#
+# m = '''1. Введение в Python
+# 2. Строки и списки
+# 3. Условные операторы
+# 4. Циклы
+# 5. Словари, кортежи и множества
+# 6. Выход'''
+#
+# print(m)
+# number = int(input())
+# if number == 1:
+#     print('1. Введение в Python')
+# elif number == 2:
+#     print('2. Строки и списки')
+# elif number == 3:
+#     print('3. Условные операторы')
+# elif number == 4:
+#     print('4. Циклы')
+# elif number == 5:
+#     print('5. Словари, кортежи и множества')
+# elif number == 6:
+#     print('6. Выход')
+# else:
+#     print('Ошибка. Введите число от 1 до 6')
 
-m = '''1. Введение в Python
-2. Строки и списки
-3. Условные операторы
-4. Циклы
-5. Словари, кортежи и множества
-6. Выход'''
 
-print(m)
-number = int(input())
-if number == 1:
-    print('1. Введение в Python')
-elif number == 2:
-    print('2. Строки и списки')
-elif number == 3:
-    print('3. Условные операторы')
-elif number == 4:
-    print('4. Циклы')
-elif number == 5:
-    print('5. Словари, кортежи и множества')
-elif number == 6:
-    print('6. Выход')
-else:
-    print('Ошибка. Введите число от 1 до 6')
-
+# Task 2
+#
+# numbers = input().split()
+# a = int(numbers[0])
+# b = int(numbers[1])
+# c = int(numbers[2])
+# min_numbers = a
+# if min_numbers > b:
+#     min_numbers = b
+# if min_numbers > c:
+#     min_numbers = c
+# print(min_numbers)
