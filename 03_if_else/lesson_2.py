6c4839a1d0f50a9740d66918457143ad185bcda5
Kristina Tarakanova <kkanadore@gmail.com>
2024-07-30 19:41:17 +0300

    Task 3

--- 03_if_else/lesson_2.py
+++ 03_if_else/lesson_2.py
@@ -37,3 +37,16 @@
 # if min_numbers > c:
 #     min_numbers = c
 # print(min_numbers)
+
+
+# Task 3
+#
+# weight = float(input())
+# if weight <= 60:
+#     print('1')
+# elif weight <= 64:
+#     print('2')
+# elif weight <= 69:
+#     print('3')
+# elif weight > 69:
+#     print('4')
