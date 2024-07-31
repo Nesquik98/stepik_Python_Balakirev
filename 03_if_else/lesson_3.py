ba2f1e344af26641e20b3bd8302954109166d01f
Kristina Tarakanova <kkanadore@gmail.com>
2024-07-31 12:37:15 +0300

    Task 2

--- 03_if_else/lesson_3.py
+++ 03_if_else/lesson_3.py
@@ -1,5 +1,12 @@
 # Task 1
+#
+# a, b = float(input()), float(input())
+# d = a if a > b else b
+# print(d)
 
-a, b = float(input()), float(input())
-d = a if a > b else b
-print(d)
+
+# Task 2
+
+a = int(input())
+msg = 'кратно 3' if a % 3 == 0 else 'не кратно 3'
+print(msg)
