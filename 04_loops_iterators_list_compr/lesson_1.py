9c0921bd8141f2723547627c8c79233f6b298b83
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-01 06:59:32 +0300

    Task 2

--- 04_loops_iterators_list_compr/lesson_1.py
+++ 04_loops_iterators_list_compr/lesson_1.py
@@ -1,8 +1,17 @@
 # Task 1
+#
+# n, m = map(int, input().split())
+# counter = n   #отслеживание текущего цикла
+# while counter <= m:
+#     print(counter ** 2, end=' ')
+#     counter += 1
 
-n, m = map(int, input().split())
-counter = n  #отслеживание текущего цикла
-while counter <= m:
-    print(counter ** 2, end=' ')
+
+# Task 2
+
+x = float(input())
+counter = 2
+while counter <= 10:
+    x_res = x * counter
+    print(f'{x_res:.1f}', end=' ')
     counter += 1
-
