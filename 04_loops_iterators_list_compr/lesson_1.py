a63bac5beb051663a84883d3c9c4b2f1acccbe21
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-01 07:40:04 +0300

    Task 3

--- 04_loops_iterators_list_compr/lesson_1.py
+++ 04_loops_iterators_list_compr/lesson_1.py
@@ -8,10 +8,21 @@
 
 
 # Task 2
+#
+# x = float(input())
+# counter = 2
+# while counter <= 10:
+#     x_res = x * counter
+#     print(f'{x_res:.1f}', end=' ')
+#     counter += 1
 
-x = float(input())
-counter = 2
-while counter <= 10:
-    x_res = x * counter
-    print(f'{x_res:.1f}', end=' ')
-    counter += 1
+
+# Task 3
+
+n = int(input())
+sum = 0
+i = 1
+while i <= n:
+    sum += 1 / i
+    i += 1
+print(f'{sum:.3f}')
