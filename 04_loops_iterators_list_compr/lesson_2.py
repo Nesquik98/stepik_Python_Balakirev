95393bb53c61ee80b679a75377ff2d2f2ba9067a
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-01 17:09:29 +0300

    Task 2

--- 04_loops_iterators_list_compr/lesson_2.py
+++ 04_loops_iterators_list_compr/lesson_2.py
@@ -1,12 +1,24 @@
 # Task 1
+#
+# p = [0] * 10
+# count = 0
+# while count < 5:
+#     index = int(input())
+#     if p[index] == 1:
+#         continue
+#     p[index] = 1
+#     count += 1
+# print(*p)
 
-p = [0] * 10
-count = 0
-while count < 5:
-    index = int(input())
-    if p[index] == 1:
-        continue
-    p[index] = 1
-    count += 1
-print(*p)
 
+# Task 2
+#
+# p = 1
+# while True:                 # запуск бесконечного цикла
+#     num = int(input())
+#     if num == 0:
+#         break
+#     if num < 0:
+#         continue
+#     p *= num
+# print(p)
