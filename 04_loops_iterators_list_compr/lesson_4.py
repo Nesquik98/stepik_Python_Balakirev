fab885cc7847a9988cac708f8660891170559e52
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-04 22:04:14 +0300

    Task 2

--- 04_loops_iterators_list_compr/lesson_4.py
+++ 04_loops_iterators_list_compr/lesson_4.py
@@ -1,16 +1,30 @@
 # Task 1
+#
+# a = input()
+# index_lst = []
+# start = 0
+# while True:
+#     start = a.find('ра', start)
+#     if start == -1:
+#         break
+#     index_lst.append(start)
+#     start += 2
+#
+# if index_lst:
+#     print(*index_lst)
+# else:
+#     print(-1)
 
-a = input()
-index_lst = []
-start = 0
-while True:
-    start = a.find('ра', start)
-    if start == -1:
-        break
-    index_lst.append(start)
-    start += 2
 
-if index_lst:
-    print(*index_lst)
-else:
-    print(-1)
+# Task 2
+#
+# number = input('Enter number')
+# if not number.startswith('+7'):
+#     print('НЕТ')
+# elif not (number[2] == '(' and number[6] == ')' and number[10] == '-' and number[13] == '-'):
+#     print('НЕТ')
+# elif not (number[3:6] + number[7:10] + number[11:13] + number[14:]).isdigit():
+#     print('НЕТ')
+# else:
+#     print('ДА')
+
