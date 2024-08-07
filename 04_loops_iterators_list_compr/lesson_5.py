c9516782f450d98d63f20babac3eaec79856f18e
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-07 20:42:42 +0300

    Task 3

--- 04_loops_iterators_list_compr/lesson_5.py
+++ 04_loops_iterators_list_compr/lesson_5.py
@@ -9,8 +9,15 @@
 
 
 # Task 2
-iterator = iter(input())
-for element in iterator:
-    if element == ' ':
-        break
-    print(element, end='')
+# iterator = iter(input())
+# for element in iterator:
+#     if element == ' ':
+#         break
+#     print(element, end='')
+
+
+# Task 3
+# number = int(input())
+# num_iter = iter(str(number))
+# for i in num_iter:
+#     print(i, end=' ')
