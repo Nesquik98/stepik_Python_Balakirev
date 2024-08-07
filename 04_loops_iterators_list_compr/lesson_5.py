524d97c24ac7a22e6650f3171f12c7922d9c9928
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-07 20:31:02 +0300

    Task 2

--- 04_loops_iterators_list_compr/lesson_5.py
+++ 04_loops_iterators_list_compr/lesson_5.py
@@ -1,8 +1,16 @@
 # Task 1
-cities = list(map(str, input().split()))
-iterator = iter(cities)
+# cities = list(map(str, input().split()))
+# iterator = iter(cities)
+#
+# for i, element in enumerate(iterator):
+#     print(element)
+#     if i == 1:
+#         break
 
-for i, element in enumerate(iterator):
-    print(element)
-    if i == 1:
+
+# Task 2
+iterator = iter(input())
+for element in iterator:
+    if element == ' ':
         break
+    print(element, end='')
