89c8d419e88d6e37d3e47dbfea1ce4a64fdb3fee
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-09 11:17:06 +0300

    Task 1

---
+++ 04_loops_iterators_list_compr/lesson_6.py
@@ -0,0 +1,15 @@
+# Task 1
+# N = int(input())
+# for i in range(N):
+#     for j in range(N):
+#         if j == N - 1:
+#             print(5, end='')
+#         else:
+#             print(1, end=' ')
+#     print()
+
+# n = int(input())
+# mtrx = [[1] * (n-1) + [5] for _ in range(n)]
+# for line in mtrx:
+#   print(*line)
+
