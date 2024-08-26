7560e79b5a5b96147d6d01135eba1f543c9fcfc7
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-26 14:49:23 +0300

    Task 3

--- 05_dictionaries_tuples_sets/lesson_1.py
+++ 05_dictionaries_tuples_sets/lesson_1.py
@@ -9,6 +9,7 @@
 # d = dict([(x[0], int(x[1])) for x in lst])
 # print(*sorted(d.items()))
 
+
 # Task 2
 # import sys
 # lst_in = list(map(str.strip, sys.stdin.readlines()))
@@ -18,3 +19,14 @@
 #     key = int(key)
 #     d[key] = value
 # print(*sorted(d.items()))
+
+
+# Task 3
+input_string = input().strip()  #удаляем пробелы вокруг строк
+pairs = input_string.split(' ')
+d = {pair.split('=')[0]: pair.split('=')[1] for pair in pairs}
+keys_to_check = ['house', 'True', '5']
+if all(key in d for key in keys_to_check):
+    print("ДА")
+else:
+    print("НЕТ")
