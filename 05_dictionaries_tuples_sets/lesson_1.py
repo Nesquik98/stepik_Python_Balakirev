5462f8449872d94149f3ab442cdd1de2f14cc68a
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-26 11:02:19 +0300

    Task 2

--- 05_dictionaries_tuples_sets/lesson_1.py
+++ 05_dictionaries_tuples_sets/lesson_1.py
@@ -3,9 +3,18 @@
 #      "tree": "дерево", "road": "дорога",
 #      "river": "река"}
 # print(d)
+#
+# lst = input().split()
+# lst = [x.split("=") for x in lst]
+# d = dict([(x[0], int(x[1])) for x in lst])
+# print(*sorted(d.items()))
 
-
-lst = input().split()
-lst = [x.split("=") for x in lst]
-d = dict([(x[0], int(x[1])) for x in lst])
-print(*sorted(d.items()))
+# Task 2
+# import sys
+# lst_in = list(map(str.strip, sys.stdin.readlines()))
+# d = {}
+# for i in lst_in:
+#     key, value = i.split("=")
+#     key = int(key)
+#     d[key] = value
+# print(*sorted(d.items()))
