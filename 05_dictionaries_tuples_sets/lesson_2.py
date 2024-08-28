d68ca9bc283700d5e1235a90d18decf9ad94bdc3
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-28 19:45:32 +0300

    Task 2

--- 05_dictionaries_tuples_sets/lesson_2.py
+++ 05_dictionaries_tuples_sets/lesson_2.py
@@ -1,15 +1,38 @@
 # Task 1
+# morse = {
+#     'А': '.-',  'Б': '-...', 'В': '.--',  'Г': '--.',  'Д': '-..',  'Е': '.',    'Ё': '.',    'Ж': '...-', 'З': '--..',
+#     'И': '..',  'Й': '.---', 'К': '-.-',  'Л': '.-..', 'М': '--',   'Н': '-.',   'О': '---',  'П': '.--.', 'Р': '.-.',
+#     'С': '...', 'Т': '-',    'У': '..-',  'Ф': '..-.', 'Х': '....', 'Ц': '-.-.', 'Ч': '---.', 'Ш': '----', 'Щ': '--.-',
+#     'Ъ': '.--.-.', 'Ы': '-.--', 'Ь': '-..-', 'Э': '..-..', 'Ю': '..--', 'Я': '.-.-', ' ': '-...-'
+# }
+# user_input = input().upper()
+# result = []
+# for i in user_input:
+#     result.append(morse[i])
+# result = ' '.join(result)
+# print(result)
+
+
+# Task 2
 morse = {
     'А': '.-',  'Б': '-...', 'В': '.--',  'Г': '--.',  'Д': '-..',  'Е': '.',    'Ё': '.',    'Ж': '...-', 'З': '--..',
     'И': '..',  'Й': '.---', 'К': '-.-',  'Л': '.-..', 'М': '--',   'Н': '-.',   'О': '---',  'П': '.--.', 'Р': '.-.',
     'С': '...', 'Т': '-',    'У': '..-',  'Ф': '..-.', 'Х': '....', 'Ц': '-.-.', 'Ч': '---.', 'Ш': '----', 'Щ': '--.-',
     'Ъ': '.--.-.', 'Ы': '-.--', 'Ь': '-..-', 'Э': '..-..', 'Ю': '..--', 'Я': '.-.-', ' ': '-...-'
 }
-user_input = input().upper()
-result = []
-for i in user_input:
-    result.append(morse[i])
-result = ' '.join(result)
+
+morse_reverse = {}
+for key, value in morse.items():
+    morse_reverse[value] = key
+code_string = input().split(' ')
+code_massage = []
+
+for i in code_string:
+    if i in morse_reverse:
+        # Получение соответствующего символа
+        decoded_char = morse_reverse[i]
+        if decoded_char == 'Ё':
+            decoded_char = 'Е'
+        code_massage.append(decoded_char.lower())
+result = ''.join(code_massage)
 print(result)
-
-
