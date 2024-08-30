2af4f3ac2f330f8e18d8b30472cdf02142df49f3
Kristina Tarakanova <kkanadore@gmail.com>
2024-08-30 10:55:02 +0300

    Task 3

--- 05_dictionaries_tuples_sets/lesson_3.py
+++ 05_dictionaries_tuples_sets/lesson_3.py
@@ -1,20 +1,26 @@
 # Task 1
-t = (3.4, -56.7)
-print(t + tuple([int(i) for i in input().split()]))
+# t = (3.4, -56.7)
+# print(t + tuple([int(i) for i in input().split()]))
 
 
 # Task 2
-cities_tuple = tuple(input().split())
-if 'Москва' not in cities_tuple:
-    cities_tuple += ('Москва',)
-print(*cities_tuple)
+# cities_tuple = tuple(input().split())
+# if 'Москва' not in cities_tuple:
+#     cities_tuple += ('Москва',)
+# print(*cities_tuple)
 
 # or
+#
+# cities_tuple = tuple(input().split())
+# cities_tuple += [(), ('Москва',)]['Москва' not in cities_tuple]
+# print(*cities_tuple)
 
-cities_tuple = tuple(input().split())
-cities_tuple += [(), ('Москва',)]['Москва' not in cities_tuple]
-print(*cities_tuple)
 
+# Task 3
+cities = tuple(input().split())
+if 'Ульяновск' in cities:
+    cities = tuple(city for city in cities if city != 'Ульяновск')
+print(*cities)
 
 
 
