c0b6e6d6347a773ee3c5f41f6d725491a05034dd
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-02 12:54:55 +0300

    Task 2

--- 05_dictionaries_tuples_sets/lesson_4.py
+++ 05_dictionaries_tuples_sets/lesson_4.py
@@ -1,3 +1,7 @@
 # Task 1
-s = list(map(float, input().split()))
-print(*sorted(s))
+# s = list(map(float, input().split()))
+# print(*sorted(s))
+
+# Task 2
+suggestion = set(list(input().lower().split()))
+print(len(suggestion))
