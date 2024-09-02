ae8bc0ddc38af5e5b145dff821355b3944f5a312
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-02 13:14:03 +0300

    Task 3

--- 05_dictionaries_tuples_sets/lesson_4.py
+++ 05_dictionaries_tuples_sets/lesson_4.py
@@ -3,5 +3,17 @@
 # print(*sorted(s))
 
 # Task 2
-suggestion = set(list(input().lower().split()))
-print(len(suggestion))
+# suggestion = set(list(input().lower().split()))
+# print(len(suggestion))
+
+
+# Task 3
+num = set()
+suggestion = input()
+for i in suggestion:
+    if i.isdigit():
+        num.add(i)
+if num:
+    print(*sorted(num))
+else:
+    print('НЕТ')
