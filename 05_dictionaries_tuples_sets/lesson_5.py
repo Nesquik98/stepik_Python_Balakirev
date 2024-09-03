3e453279fca794cd2dbcc28f9e81b3bb3927fe14
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-03 12:12:24 +0300

    Task 2

--- 05_dictionaries_tuples_sets/lesson_5.py
+++ 05_dictionaries_tuples_sets/lesson_5.py
@@ -4,3 +4,7 @@
 # s = set(num1) & set(num2)
 # print(*sorted(s))
 
+# Task 2
+# num1 = set(list(map(int, input().split())))
+# num2 = set(list(map(int, input().split())))
+# print(*sorted(num1 - num2))
