a1efed20e75f63075de4785f6d603bcb40c1d3ab
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-07 20:42:07 +0300

    Task 4 function/finding values from the list

--- 06_functions/lesson_1.py
+++ 06_functions/lesson_1.py
@@ -17,9 +17,20 @@
 
 
 # Task 3
-def sent_mail():
-    text = f"Предмет имеет вес: {input()} кг."
-    print(text)
+# def sent_mail():
+#     text = f"Предмет имеет вес: {input()} кг."
+#     print(text)
+#
+#
+# sent_mail()
 
 
-sent_mail()
+# Task 4
+def calculate_metrix(x):
+    v_min = min(x)
+    v_max = max(x)
+    v_sum = sum(x)
+    print(f"Min = {v_min}, max = {v_max}, sum = {v_sum}")
+
+
+calculate_metrix([int(x) for x in input().split()])
