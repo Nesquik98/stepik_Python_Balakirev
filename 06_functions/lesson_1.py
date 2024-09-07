f6de65b34bcc1749b48392a7a03c4aa87ab6d797
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-07 20:04:26 +0300

    Task 2 function with input stream

--- 06_functions/lesson_1.py
+++ 06_functions/lesson_1.py
@@ -1,7 +1,16 @@
 # Task 1
-def send_mail():
-    text = "It's my first function"
+# def send_mail():
+#     text = "It's my first function"
+#     print(text)
+#
+#
+# send_mail()
+
+
+# Task 2
+def sent_mail():
+    text = f"Уважаемый, {input()}! Вы верно выполнили это задание!"
     print(text)
 
 
-send_mail()
+sent_mail()
