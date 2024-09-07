5f0ddcf3e3d6fbfebd895232dd0a1f2a5bc0c482
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-07 12:05:55 +0300

    Task 2

--- 05_dictionaries_tuples_sets/lesson_6.py
+++ 05_dictionaries_tuples_sets/lesson_6.py
@@ -6,3 +6,10 @@
 # d = {grade1 + i: grade for i, grade in enumerate(grades)}
 #
 # print(d[4])
+
+
+# Task 2
+# import sys
+# lst_in = list(map(str.strip, sys.stdin.readlines()))
+# print(len({number for number in lst_in}))
+
