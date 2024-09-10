f121ee52416428b17130dbd208c9ab7f9aac55f2
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-10 15:43:10 +0300

    Task 2 function/return + bool

--- 06_functions/lesson_2.py
+++ 06_functions/lesson_2.py
@@ -1,10 +1,17 @@
 # Task 1
-def get_sq(x):
-    return x ** 2
+# def get_sq(x):
+#     return x ** 2
+#
+#
+# num = float(input())
+#
+# result = get_sq(num)
+# print(result)
 
 
-num = float(input())
-
-result = get_sq(num)
-print(result)
-
+# Task 2
+def is_triangle(*args) -> bool:
+    if len(*args) != 3:
+        return False
+    a, b, c = args
+    return abs(a - b) < c and abs(b - c) < a and abs(c - a) < b
