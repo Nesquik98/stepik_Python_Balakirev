93ed46c4370b3bbf38a2e4c9f88aa13b97e6f464
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-17 18:07:43 +0300

    Task 2 args/return key=len/max

--- 06_functions/lesson_4.py
+++ 06_functions/lesson_4.py
@@ -1,3 +1,16 @@
 # Task 1
-def get_even(*args):
-    return [num for num in args if num % 2 == 0]
+# def get_even(*args):
+#     return [num for num in args if num % 2 == 0]
+
+# Task 2
+def get_biggest_city(*cities):
+    biggest_city = ''
+    for city in cities:
+        if len(city) > len(biggest_city):
+            biggest_city = city
+
+    return biggest_city
+
+
+def get_biggest_city(*cities) -> str:
+    return max(cities, key=len)
