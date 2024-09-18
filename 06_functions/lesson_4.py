f183c245499f7113984daa895aeb6cbf9064b687
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-18 12:27:04 +0300

    Task 3 args/kwargs

--- 06_functions/lesson_4.py
+++ 06_functions/lesson_4.py
@@ -3,14 +3,30 @@
 #     return [num for num in args if num % 2 == 0]
 
 # Task 2
-def get_biggest_city(*cities):
-    biggest_city = ''
-    for city in cities:
-        if len(city) > len(biggest_city):
-            biggest_city = city
+# def get_biggest_city(*cities):
+#     biggest_city = ''
+#     for city in cities:
+#         if len(city) > len(biggest_city):
+#             biggest_city = city
+#
+#     return biggest_city
+#
+#
+# def get_biggest_city(*cities) -> str:
+#     return max(cities, key=len)
 
-    return biggest_city
+# Task 3
+def get_data_fig(*args, **kwargs):
+    perimetr = sum(args)
+    result = (perimetr,)
 
+    if 'tp' in kwargs:
+        result += (kwargs['tp'],)
+    if 'color' in kwargs:
+        result += (kwargs['color'],)
+    if 'closed' in kwargs:
+        result += (kwargs['closed'],)
+    if 'width' in kwargs:
+        result += (kwargs['width'],)
 
-def get_biggest_city(*cities) -> str:
-    return max(cities, key=len)
+    return result
