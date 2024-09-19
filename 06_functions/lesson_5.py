ca84a22fe91caaf410bf108a4f5043cf351266fd
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-19 11:32:40 +0300

    Task 2 unpacking/converting a list into a tuple

--- 06_functions/lesson_5.py
+++ 06_functions/lesson_5.py
@@ -1,6 +1,11 @@
 # Task 1
-input_numbers = map(int, input().split())
-*lst, x, y, z = input_numbers
-print(*lst)
+# input_numbers = map(int, input().split())
+# *lst, x, y, z = input_numbers
+# print(*lst)
+
+# Task 2
+input_cities = input().split()
+lst_c = (*input_cities,)
+print(lst_c)
 
 
