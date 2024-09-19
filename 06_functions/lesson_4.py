38b29fc018578143455ae4a1b9dc3054d73fc468
Nesquik98 <kkanadore@gmail.com>
2024-09-19 09:33:45 +0300

    Functins with an arbitrary number of parameters (#17)
    * Task 1 args/return even numbers
    
    * Task 2 args/return key=len/max
    
    * Task 3 args/kwargs
    
    * Task 4 matrix/checking an isolated number
    
    * Task 4 functions/finding th smallest among 4 variants

---
+++ 06_functions/lesson_4.py
@@ -0,0 +1,61 @@
+# Task 1
+# def get_even(*args):
+#     return [num for num in args if num % 2 == 0]
+
+# Task 2
+# def get_biggest_city(*cities):
+#     biggest_city = ''
+#     for city in cities:
+#         if len(city) > len(biggest_city):
+#             biggest_city = city
+#
+#     return biggest_city
+#
+#
+# def get_biggest_city(*cities) -> str:
+#     return max(cities, key=len)
+
+# Task 3
+# def get_data_fig(*args, **kwargs):
+#     perimetr = sum(args)
+#     result = (perimetr,)
+#
+#     if 'tp' in kwargs:
+#         result += (kwargs['tp'],)
+#     if 'color' in kwargs:
+#         result += (kwargs['color'],)
+#     if 'closed' in kwargs:
+#         result += (kwargs['closed'],)
+#     if 'width' in kwargs:
+#         result += (kwargs['width'],)
+#
+#     return result
+
+
+# Task 4
+# def verify(x):
+#     flag = True
+#     for i in range(len(x)-1):
+#         for j in range(len(x)-1):
+#             # если сумма 4х смежных элементов больше 1, значит найдени квадрат 2х2 в котором
+#             # хотя бы два элемента больше нуля
+#             if x[i][j] + x[i][j+1] + x[i+1][j] + x[i+1][j+1] > 1:
+#                 flag = False
+#                 break
+#     return flag
+
+
+# Task 5
+# def str_min(s1, s2):
+#     if s1 < s2:
+#         return s1
+#     else:
+#         return s2
+#
+#
+# def str_min3(s1, s2, s3):
+#     return str_min(str_min(s1, s2), s3)
+#
+#
+# def str_min4(s1, s2, s3, s4):
+#     return str_min(str_min(s1, s2), str_min(s3, s4))
