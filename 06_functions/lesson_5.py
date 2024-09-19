c062b202cd4e937fcdaff59ba2b67b2780ef2cf3
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-19 19:29:42 +0300

    Task 3 recursive function/ fibonacci num sequence

--- 06_functions/lesson_5.py
+++ 06_functions/lesson_5.py
@@ -9,12 +9,22 @@
 # get_rec_N(N)
 
 # Task 2
-def get_rec_sum(nums: list[int]) -> int:
-    if not nums:
-        return 0
-    nums[1] += nums[0]
-    return get_rec_sum(nums[1:])
+# def get_rec_sum(nums: list[int]) -> int:
+#     if not nums:
+#         return 0
+#     nums[1] += nums[0]
+#     return get_rec_sum(nums[1:])
+#
+#
+# nums = list(map(int, input().split()))
+# print(get_rec_sum(nums))
+
+# Task 3
+N = int(input())
 
 
-nums = list(map(int, input().split()))
-print(get_rec_sum(nums))
+def fib_rec(N, f=[1, 1]):
+    if len(f) == N:
+        return f
+    f.append(f[-1] + f[-2])
+    return fib_rec(N, f)
