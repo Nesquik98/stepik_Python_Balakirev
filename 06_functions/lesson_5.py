a8122ed5f26de9bcbe33754e7e17488c839632ba
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-19 19:11:06 +0300

    Task 2 recursive function/ sum num

--- 06_functions/lesson_5.py
+++ 06_functions/lesson_5.py
@@ -1,9 +1,20 @@
 # Task 1
-def get_rec_N(n):
-    if n > 0:
-        get_rec_N(n - 1)
-        print(n)
+# def get_rec_N(n):
+#     if n > 0:
+#         get_rec_N(n - 1)
+#         print(n)
+#
+#
+# N = int(input())
+# get_rec_N(N)
+
+# Task 2
+def get_rec_sum(nums: list[int]) -> int:
+    if not nums:
+        return 0
+    nums[1] += nums[0]
+    return get_rec_sum(nums[1:])
 
 
-N = int(input())
-get_rec_N(N)
+nums = list(map(int, input().split()))
+print(get_rec_sum(nums))
