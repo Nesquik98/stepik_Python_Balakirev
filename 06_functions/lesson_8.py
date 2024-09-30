538f3329eeab1e53e6480e428557a2e685924a6d
Kristina Tarakanova <kkanadore@gmail.com>
2024-09-30 20:13:50 +0300

    Task 2 decorator/function/@ operator (convert the passed string into a numbered list of words and return the list)

--- 06_functions/lesson_8.py
+++ 06_functions/lesson_8.py
@@ -1,11 +1,29 @@
-# Task 1
-def func_show(func):
-    def wrapper(width, height):
-        p = func(width, height)
-        print(f"Площадь прямоугольника: {p}")
-        return p
-    return wrapper
+# Task 1 created a function and defined a decorator for that function
+# def func_show(func):
+#     def wrapper(width, height):
+#         p = func(width, height)
+#         print(f"Площадь прямоугольника: {p}")
+#         return p
+#     return wrapper
+#
+#
+# def get_sq(width, height):
+#     return width * height
 
 
-def get_sq(width, height):
-    return width * height
+# Task 2 decorator/function/@ operator (convert the passed string into a numbered list of words and return the list)
+# def show_menu(func):
+#     def wrapper(*args):
+#         items = func(*args)
+#         for i, item in enumerate(items, 1):
+#             print(f"{i}. {item}")
+#         return items
+#     return wrapper
+#
+#
+# @show_menu
+# def get_menu(s):
+#     return s.split()
+#
+# menu = input()
+
